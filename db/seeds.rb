610f0ab29dfe749168a64a37360d5492cdcf7cc9
Basile Rieu <basilerieu@yahoo.fr>
2021-04-15 16:28:43 +0200

    Projects displayed basically, ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,48 @@
   Project.create(
     text: "assets/Sp2txt-768.png",
     image: "assets/Sp2img-768.png",
-    illustration: "assets/Sp3-1920.png"
+    illustration: "assets/Sp3-1920.png",
+    name: "Pithagore, RATP"
   )
 
   Project.create(
     text: "assets/Sp4txt-768.png",
     image: "assets/Sp4img-768.png",
-    illustration: "assets/Sp5-1920.png"
+    illustration: "assets/Sp5-1920.png",
+    name: "Smart couponing, HighcoBox"
   )
 
   Project.create(
     text: "assets/Sp6txt-768.png",
     image: "assets/Sp6img-768.png",
-    illustration: "assets/Sp7-1920.png"
+    illustration: "assets/Sp7-1920.png",
+    name: "Concept car, Voyages-sncf.com"
   )
 
   Project.create(
     text: "assets/Sp8txt-768.png",
     image: "assets/Sp8img-768.png",
-    illustration: "assets/Sp9-1920.png"
+    illustration: "assets/Sp9-1920.png",
+    name: "NVOCC manager, Bolloré"
   )
 
   Project.create(
     text: "assets/Sp10txt-768.png",
     image: "assets/Sp10img-768.png",
-    illustration: "assets/Sp11-1920.png"
+    illustration: "assets/Sp11-1920.png",
+    name: "Mes services pour la maison, LaPoste"
   )
 
   Project.create(
     text: "assets/Sp12txt-768.png",
     image: "assets/Sp12img-768.png",
-    illustration: "assets/Sp13-1920.png"
+    illustration: "assets/Sp13-1920.png",
+    name: "L'appli TER, SNCF"
   )
 
   Project.create(
     text: "assets/Sp14txt-768.png",
     image: "assets/Sp14img-768.png",
-    illustration: "assets/Sp15-1920.png"
+    illustration: "assets/Sp15-1920.png",
+    name: "Coach éco-conduite, AXA"
   )
