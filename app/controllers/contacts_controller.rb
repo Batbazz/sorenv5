1d90b3052b8e207c89cea01d36b268885bdad144
Basile Rieu <basilerieu@yahoo.fr>
2021-05-03 16:10:48 +0200

    Add document.location.reload() to the method for btnsubmit/animation out, and deleted the e.PreventDefault because it blocked the sendings of mails, go figure...

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,13 +3,14 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
-      ContactMailer.with(contact: @contact).new_contact_email.deliver_now
+      ContactMailer.with(contact: @contact).new_contact_email.deliver_later
 
       flash[:success] = "Merci pour votre message ! Je vous répondrai dans les plus brefs délais."
       redirect_to root_path
     else
-      flash.now[:error] = "Il manque au moins une information à votre message."
-      render 'pages/home'
+      flash[:error] = "Il manque au moins une information à votre message."
+      # render 'pages/home'
+      redirect_to root_path
     end
   end
 
--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -8,19 +8,21 @@ const modalid = document.getElementById('modalId');
 const modalclass = document.querySelector('modal');
 
 // Get the submit button
-const btnsubmit = document.getElementById('btnsubmit');
+const btnsubmit = document.getElementById('_btnsubmit');
 
-// When the user clicks on the button, open the modal
-btn.addEventListener('click', (event) => {
+// When the user clicks on the button, it opens the modal
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
   modalid.style.display = "block";
 });
 
-// When the user clicks on the submit button, close the modal and animate it
-btnsubmit.addEventListener('click', (event) => {
+// When the user clicks on the submit button, it closes the modal and animate it while sending mail
+btnsubmit.addEventListener('click', (e) => {
   modalid.classList.add('animate__animated', 'animate__backOutUp', 'animate__faster');
+  document.location.reload();
 });
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, it closes it
 window.addEventListener('click', (event) => {
   if (event.target == modalid) {
     modalid.style.display = "none";
