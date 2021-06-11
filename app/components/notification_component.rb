9e96e045ab2d0f26713fe38fa9a9d8eff8b52da1
Basile Rieu <basilerieu@yahoo.fr>
2021-06-11 16:59:55 +0200

    Just a save

--- app/components/notification_component.rb
+++ app/components/notification_component.rb
@@ -2,10 +2,41 @@ class NotificationComponent < ViewComponent::Base
   def initialize(type:, data:)
     @type = type
     @data = prepare_data(data)
+
+    @icon_class = icon_class
+    @icon_color_class = icon_color_class
+
+    @data[:timeout] ||= 3
   end
 
 
   private
+
+  def icon_class
+    case @type
+    when 'success'
+      'fa-check-square'
+    when 'error'
+      'fa-exclamation-square'
+    when 'alert'
+      'fa-exclamation-square'
+    else
+      'fa-info-square'
+    end
+  end
+
+  def icon_color_class
+    case @type
+    when 'success'
+      'text-green-400'
+    when 'error'
+      'text-red-800'
+    when 'alert'
+      'text-red-400'
+    else
+      'text-gray-400'
+    end
+  end
 
   def prepare_data(data)
     case data
@@ -15,5 +46,4 @@ class NotificationComponent < ViewComponent::Base
       { title: data }
     end
   end
-
 end
